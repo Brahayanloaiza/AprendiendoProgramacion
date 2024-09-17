9a2baeb8215ba5dcc3b53d12a2d1de6596f5d0b2
Brahayan Alonso Loaiza Chacón <brahayan.loaiza@uamcr.net>
2024-09-17 13:42:13 -0600

    agrege el archivo index.html

---
+++ Aprendiendo a programar.go
@@ -0,0 +1,7 @@
+Aprendiendo a programar
+
+tipos de datos
+
+"int" o interger es para numeros enteros (2,4,19,etc)
+"floar" o flotantes es para numeros con decimales (1.1, 2.5, 3.1415. etc)
+"booleans" o buleanos si son (Verdadero, falso) (true, false),
